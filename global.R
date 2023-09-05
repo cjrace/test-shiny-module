986a9a3db1d2b23db7773527ad133419aa9eaa24
cjrace <52536248+cjrace@users.noreply.github.com>
2023-09-05 14:27:00 +0100

    add dplyr and extra datasets

--- global.R
+++ global.R
@@ -2,12 +2,13 @@
 
 library(shiny)
 library(DT)
+library(dplyr)
 
 # Read in data sets to use =====================================================
 ## Geographic data ----
 lake_huron_dataset <- LakeHuron
-# nile_dataset <- Nile
+nile_dataset <- Nile
 
 ## Vehicle data ----
 cars_dataset <- cars
-# cars_testing_dataset <- mtcars
+cars_testing_dataset <- mtcars
