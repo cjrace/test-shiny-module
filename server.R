2a6026aa26c07b0296848a4c00196884e5b8863a
cjrace <52536248+cjrace@users.noreply.github.com>
2023-09-05 13:46:36 +0100

    fix typo in input value

--- server.R
+++ server.R
@@ -3,9 +3,9 @@ server <- function(input, output, session) {
   user_selection <- user_selection_server(id = "user_selection")
   
   dataset <- reactive({
-    if(user_selection == "vehicles"){
+    if(user_selection() == "vehicles"){
       return(cars_dataset)
-    } else if (user_selection == "geographic") {
+    } else if (user_selection() == "geographic") {
       return(lake_huron_dataset)
     } 
   })
